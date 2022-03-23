fa16fd658e030d1e5480ce6ab65d95c8d2172fb4
chaenabi <find1086@gmail.com>
2022-03-23 17:03:20 +0900

    add get item swagger

--- src/main/kotlin/com/kiosk/api/item/controller/ItemController.kt
+++ src/main/kotlin/com/kiosk/api/item/controller/ItemController.kt
@@ -7,6 +7,7 @@ import com.kiosk.api.item.domain.enums.ItemMessage
 import com.kiosk.api.item.service.ItemService
 import com.kiosk.exception.item.InvalidItemParameterException
 import com.kiosk.exception.item.ItemCrudErrorCode
+import io.swagger.annotations.*
 import org.apache.commons.io.IOUtils
 import org.springframework.http.HttpStatus
 import org.springframework.validation.BindingResult
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile
 import java.util.*
 import javax.validation.Valid
 
+@Api(tags = ["ItemController"], description = "상품 API")
 @RestController
 @RequestMapping("/v1")
 class ItemController(
@@ -23,11 +25,14 @@ class ItemController(
 
     private lateinit var itemImages: MutableList<MultipartFile>
 
+    @ApiOperation("상품 정보를 받아 데이터베이스에 해당 상품을 등록합니다. 상품을 등록할 때는 카테고리 정보가 필요하지 않습니다.")
     @PostMapping("/item")
     fun saveItem(
         @Valid @RequestPart(value = "data")
+        @ApiParam(name = "data", value = "상품 정보", required = true)
         item: ItemRequestDTO.Save,
         @RequestPart(value = "attachImages", required = false)
+        @ApiParam(name = "attachImages", value = "상품 이미지", defaultValue = "[]", type = "List<MultipartFile>", required = false)
         attachImages: MutableList<MultipartFile>? = null,
         result: BindingResult,
     ): ResponseDTO<ItemResponseDTO> {
@@ -49,9 +54,13 @@ class ItemController(
         return ResponseDTO(itemService.update(item, itemImages), ItemMessage.SUCCESS_UPDATE, HttpStatus.OK)
     }
 
+    @ApiImplicitParams(
+        ApiImplicitParam(name = "page", value = "찾으려는 페이지 번호", dataType = "Int", required = true, paramType = "query", example = "1"),
+        ApiImplicitParam(name = "pageSize", value = "찾으려는 상품의 갯수", defaultValue = "10", dataType = "Int", required = false, paramType = "query", example = "10")
+    )
     @GetMapping("/item")
-    fun findItemPage(@RequestParam page: Int, @RequestParam pageSize: Int): ResponseDTO<ItemResponseDTO.Paging> {
-        return ResponseDTO(itemService.findItemPage(page, pageSize), ItemMessage.SUCCESS_FIND_AS_PAGE, HttpStatus.OK)
+    fun findItemPage(@RequestParam page: Int, @RequestParam(required = false) pageSize: Int?): ResponseDTO<ItemResponseDTO.Paging> {
+        return ResponseDTO(itemService.findItemPage(page, pageSize ?: 10), ItemMessage.SUCCESS_FIND_AS_PAGE, HttpStatus.OK)
     }
 
     @DeleteMapping("/item/{id}")
@@ -59,5 +68,6 @@ class ItemController(
         itemService.delete(id)
         return ResponseDTO(ItemMessage.SUCCESS_DELETE_ONE, HttpStatus.OK)
     }
+
 
 }
--- src/main/kotlin/com/kiosk/api/item/domain/model/ItemRequestDTO.kt
+++ src/main/kotlin/com/kiosk/api/item/domain/model/ItemRequestDTO.kt
@@ -2,12 +2,16 @@ package com.kiosk.api.item.domain.model
 
 import com.fasterxml.jackson.annotation.JsonIncludeProperties
 import com.kiosk.api.item.domain.entity.Item
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.beans.factory.annotation.Value
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
+@ApiModel(description = "상품 등록 및 상품 정보 수정 시 필요한 정보")
 class ItemRequestDTO {
 
+    @ApiModel(description = "상품 등록에 필요한 정보")
     class Save @JsonIncludeProperties("itemName", "detail", "price", "quantity") constructor(
         val itemName: String,
         val detail: String?,
@@ -25,6 +29,7 @@ class ItemRequestDTO {
     }
 
     class Update(
+        @ApiModelProperty(notes = "상품 번호는 필수입니다.")
         @field:Positive(message = "상품 번호가 반드시 전달되어야 합니다.")
         val id: Long?,
         val itemName: String?,
